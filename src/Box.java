15ef08cca0c6a5a3ab6621c82b7ff955eef1356e
unknown <albertjbelardino@gmail.com>
2018-08-30 21:47:07 -0700

    added layers to box, whitespace field

--- src/Box.java
+++ src/Box.java
@@ -1,6 +1,12 @@
 public class Box {
 
     int width;
+    int whiteSpace;
+
+    public Box(int width, int whiteSpace) {
+        this.width = width;
+        this.whiteSpace = whiteSpace;
+    }
 
     public Box(int width) {
         this.width = width;
@@ -12,6 +18,10 @@ public class Box {
 
     public void setWidth(int width) {
         this.width = width;
+    }
+
+    public void setWhiteSpace(int whiteSpace) {
+        this.whiteSpace = whiteSpace;
     }
 
     public void printHorizontalBorder() {
@@ -36,18 +46,21 @@ public class Box {
     }
 
     public void printTextInBox(String message) {
-        for(int i = 0; i < message.length(); i += width) {
-            print("*  ");
-            if(i + width - 6 < message.length()) {
-                print(message.substring(i, i += width - 6));
-            }
-            else {
-                print(message.substring(i, message.length() - 1));
-                for(int k = message.length() - i; (k + 5) % width != 0; k++)
-                    print(" ");
-            }
-            print("  *\n");
+        for(int i = 0; i < message.length(); i += width)
+            printBoxLayer(message, i);
+    }
+
+    public void printBoxLayer(String message, int i) {
+        print("*  ");
+        if(i + width - 6 < message.length()) {
+            print(message.substring(i, i += width - 6));
         }
+        else {
+            print(message.substring(i, message.length() - 1));
+            for(int k = message.length() - i; (k + 5) % width != 0; k++)
+                print(" ");
+        }
+        print("  *\n");
     }
 
     public void print(Object obj) {
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ public class Main {
     public static void main(String[] args) {
         Box myBox = new Box();
 
-        myBox.printMessageInBox("pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo" +
-                "pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo" +
-                "pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo" +
-                "pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo" +
-                "pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo");
+        String message = "";
+        for(int i = 0; i < 200; i++)
+            message += "statetsttdeteet";
+        myBox.printMessageInBox(message);
 
         myBox.setWidth(60);
         myBox.printMessageInBox("pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo" +
                 "pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo" +
                 "pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo" +
                 "pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo" +
-                "pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo");
+                "pioshpojwpoejfpjpojnpvfvfopofejjwopejfowemjpofjwpeojfwpeojfpowejfowempowpfjwjo" +
+                "jfwpeojfpowejfowempowpf");
     }
 }
